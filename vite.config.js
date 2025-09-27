f857b3495173a522c38bdd883dda15e62e4fc4dc
BlackRubick <blackrubick14@gmail.com>
2025-09-27 00:33:58 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default {
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 3000, // Puedes cambiar a 80 si lo prefieres y tienes permisos
     proxy: {
       '/v1': {
         target: 'https://sandbox.factura.com/api',
