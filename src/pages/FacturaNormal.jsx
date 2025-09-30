67e2d599d4ab551f08a8719d39a7422261b9c2f3
BlackRubick <blackrubick14@gmail.com>
2025-09-30 02:12:34 -0600

    first commit

--- src/pages/FacturaNormal.jsx
+++ src/pages/FacturaNormal.jsx
@@ -3,7 +3,6 @@ import CFDIForm from '../components/forms/CFDIForm';
 
 const FacturaNormal = () => (
   <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-    <h1 className="text-3xl font-extrabold mb-8 text-blue-700 text-center">Factura Normal</h1>
     <div className="bg-white rounded-xl shadow p-6">
       <CFDIForm />
     </div>
