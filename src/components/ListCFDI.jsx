e06dc7cd59e284fc9173b0f92208ca1b3864ee72
BlackRubick <blackrubick14@gmail.com>
2025-09-30 02:32:30 -0600

    first commit

--- src/components/ListCFDI.jsx
+++ src/components/ListCFDI.jsx
@@ -100,8 +100,9 @@ const ListCFDI = () => {
             <tr key={c.UID} className="border-b border-blue-50 hover:bg-blue-50">
               <td className="p-2">{c.Folio}</td>
               <td className="p-2">{c.RFC || c.ReceptorRFC || c.EmisorRFC || '-'}</td>
-              <td className="p-2">{c.Nombre || c.ReceptorNombre || c.EmisorNombre || c.RazonSocial || '-'}</td>
-              <td className="p-2">{c.Fecha || c.FechaEmision || c.FechaTimbrado || '-'}</td>
+              <td className="p-2">{c.Receptor || '-'}</td>
+              <td className="p-2">{c.RazonSocialReceptor || '-'}</td>
+              <td className="p-2">{c.FechaTimbrado || '-'}</td>
               <td className="p-2">${c.Total}</td>
               <td className="p-2">{c.Status}</td>
               <td className="p-2">
